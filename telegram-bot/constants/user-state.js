cb4833b9e74cd12607e419504d62fa648c81d47f
Nikita Kondratev <highflyer16@yandex.ru>
2018-04-18 12:12:57 +0300

    Feature. Add USER_STATE constants

---
+++ telegram-bot/constants/user-state.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  IDLE: 'idle',
+  CREATE_USER_ENTER_USERNAME: 'create_user_enter_username',
+  CREATE_USER_ENTER_PASSWORD: 'create_user_enter_password',
+  DELETE_USER_ENTER_USERNAME: 'delete_user_enter_username'
+})
